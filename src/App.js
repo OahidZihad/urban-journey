d97d1b512a74f36137c37f4339460156e99f5094
OahidZihad <oahidzihad1@gmail.com>
2021-06-28 01:17:01 +0600

    Add images and figma

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    color: "white",
+    backgroundColor: "tomato",
+  },
+  buttonSize: {
+    borderRadius: "50px",
+    height: "75px",
+    width: "75px",
+    color: "white",
+    backgroundColor: "tomato",
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
       <header className="App-header">
+        <Button variant="contained">Default</Button>
+        <br />
+        <Button className={classes.button} variant="contained">
+          Primary
+        </Button>
+        <br />
+        <Button variant="outlined" color="secondary">
+          Secondary
+        </Button>
+        <br />
+        <Button className={classes.buttonSize} variant="contained">
+          Disabled
+        </Button>
+        <br />
+        <Button variant="text" color="primary" href="#contained-buttons">
+          Link
+        </Button>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
