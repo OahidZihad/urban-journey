a04ded22c6ff1b6dc8ee2eb99eedff9e0757f53e
OahidZihad <oahidzihad1@gmail.com>
2021-07-06 02:53:55 +0600

    direction in google map

--- src/Components/VehicleDetail/VehicleDetail.js
+++ src/Components/VehicleDetail/VehicleDetail.js
@@ -33,7 +33,43 @@ const VehicleDetail = () => {
       </div>
       <LoadScript googleMapsApiKey="AIzaSyCVRJzWxAVF3_K6k7TRaAz1S83fRX_Vmw4">
         <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
-          <Marker position={center} onLoad={onLoad}></Marker>
+          {/* <Marker position={center} onLoad={onLoad}></Marker> */}
+          <DirectionsService
+            // required
+            options={{
+              destination: "Al Modina Super Market Gaibandha Bangladesh",
+              origin: "Gaibandha Fire Station Gaibandha Bangladesh",
+              travelMode: "DRIVING",
+            }}
+            // required
+            callback={(res) => {
+              if (res !== null) {
+                setDirectionResponse(res);
+              }
+            }}
+          />
+          {directionResponse && (
+            <DirectionsRenderer
+              // required
+              options={{
+                directions: directionResponse,
+              }}
+              // optional
+              onLoad={(directionsRenderer) => {
+                console.log(
+                  "DirectionsRenderer onLoad directionsRenderer: ",
+                  directionsRenderer
+                );
+              }}
+              // optional
+              onUnmount={(directionsRenderer) => {
+                console.log(
+                  "DirectionsRenderer onUnmount directionsRenderer: ",
+                  directionsRenderer
+                );
+              }}
+            />
+          )}
         </GoogleMap>
       </LoadScript>
     </div>
